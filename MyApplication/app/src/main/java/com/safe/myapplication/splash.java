30c38f52a032f8c5590f8395166aad205ef03d5c
sarim mumtaz <sarim mumtaz>
2016-03-06 16:32:24 +0500

    splash screen added with new project

---
+++ MyApplication/app/src/main/java/com/safe/myapplication/splash.java
@@ -0,0 +1,7 @@
+package com.safe.myapplication;
+
+/**
+ * Created by sarim on 3/6/2016.
+ */
+public class splash {
+}
---
+++ Splash/app/src/main/java/com/safe/splash/Splash.java
@@ -0,0 +1,7 @@
+package com.safe.splash;
+
+/**
+ * Created by sarim on 3/6/2016.
+ */
+public class Splash {
+}
