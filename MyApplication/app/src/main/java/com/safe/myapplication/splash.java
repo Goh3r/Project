1b02dcd90e67665d069dfb24321f688d7b3cd7e5
sarim mumtaz <sarim mumtaz>
2016-03-06 16:33:46 +0500

    new project added as splash containing splash screen

--- MyApplication/app/src/main/java/com/safe/myapplication/splash.java
+++ MyApplication/app/src/main/java/com/safe/myapplication/splash.java
@@ -1,7 +1,15 @@
 package com.safe.myapplication;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by sarim on 3/6/2016.
  */
-public class splash {
+public class splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
+    }
 }
