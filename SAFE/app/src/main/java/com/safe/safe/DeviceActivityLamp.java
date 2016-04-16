e05c8bd8fd037b499fa0d1bfb48bcc682d5433f2
sarim mumtaz <sarim mumtaz>
2016-04-16 22:53:28 +0500

    a new app added
    for the devices

---
+++ SAFE/app/src/main/java/com/safe/safe/DeviceActivityLamp.java
@@ -0,0 +1,13 @@
+package com.safe.safe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceActivityLamp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_activity_lamp);
+    }
+}
